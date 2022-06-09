3e0e76a1d0622fc3b15d376b1885512badb91eb4
Jennifer Villalobos <jennifervfdz@gmail.com>
2022-06-09 12:57:55 -0600

    Agregados los estilos.

--- puravidanft/tailwind.config.js
+++ puravidanft/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fondoTransparente: "#373a47",
+      }
+    },
   },
   plugins: [],
 }
