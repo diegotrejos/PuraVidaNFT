21a481112791d43f30c2a6b59e5a973fce076dfc
Jennifer Villalobos <jennifervfdz@gmail.com>
2022-06-24 00:54:41 -0600

    Views of Dayana restored.

--- puravidanft/src/Pages/ChangePassword/index.js
+++ puravidanft/src/Pages/ChangePassword/index.js
@@ -1,58 +1,84 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
-
+import Navbar from "../../Component/Navbar/index.js";
 
 export default function EditAccount() {
-    const navigate = useNavigate();
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setconfirmPassword] = useState("");
-    const theme = useSelector((state) => state.app.theme);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const theme = useSelector((state) => state.app.theme);
 
-    return ( 
-        <div className="flex items-center justify-center h-screen">
-        <div className="border">
-          <div className="text-center">
-            <div className="mb-8">
-              <h1 className="text-3xl font-bold">Ingrese la nueva contraseña:</h1>
-            </div>
-            <div className="mb-6">
-              <input
-                placeholder="Contraseña"
-                className={`placeholder:text-black pl-4 h-[48px] w-full rounded-md ${theme.inputBg} ${theme.inputText}`}
-                type="password"
-                value={password}
-                onChange={(evt) => {
-                  setPassword(evt.target.value);
-                }}
-              />
-            </div>
-            <div className="mb-6">
-              <input
-                placeholder="Confirmar contraseña"
-                className={`placeholder:text-black pl-4 h-[48px] w-full rounded-md ${theme.inputBg} ${theme.inputText}`}
-                type="password"
-                value={confirmPassword}
-                onChange={(evt) => {
-                    setconfirmPassword(evt.target.value);
-                }}
-              />
-            </div>
-  
-            <div className="mb-6">
-            <button className="h-[48px] w-[200px] rounded-md bg-purple-500 text-white">
-              Confirmar Cambios
+  return (
+    <div class="grid-rows-2">
+      <Navbar />
+      <div class="flex m-10 mr-4">
+        <div class="w-1/2 p-8">
+          <div className="mb-12 text-center ">
+            <h1 className=" text-3xl font-bold">Editar Cuenta :</h1>
+          </div>
+
+          <div className="mb-12 text-center">
+            <input
+              placeholder="Nombre"
+              className={`placeholder:text-black pl-4 h-[48px] w-[400px] rounded-md ${theme.inputBg} ${theme.inputText}`}
+              value={name}
+              onChange={(evt) => {
+                setName(evt.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-12  text-center">
+            <input
+              placeholder="Email"
+              className={`placeholder:text-black pl-4 h-[48px] w-[400px] rounded-md ${theme.inputBg} ${theme.inputText}`}
+              value={email}
+              onChange={(evt) => {
+                setEmail(evt.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-4 text-center">
+            <button
+              className="h-[48px] w-[300px] rounded-md bg-gray-500 text-white justify-center "
+              onClick={() => navigate("/chancepassword")}
+            >
+              Cambiar contraseña
             </button>
           </div>
-            <div className="mb-4">
-              <button className="h-[48px] w-[200px] rounded-md bg-gray-500 text-white"
-              onClick={() => navigate("/editaccount")}>
-                Cancelar
-              </button>
-            </div>
+        </div>
+        <div class="w-1/2 p-4">
+          <div className="mb-4 flex justify-center ">
+            <img
+              className={`${"w-100"} ${"h-80"}`}
+              alt="Imagen de Usuario"
+              src="https://i.pinimg.com/736x/a6/5e/ba/a65ebae219c97f8624f32140e80889a9.jpg"
+            />
+          </div>
+          <div className="mb-4 flex justify-center ">
+            <button className="h-[48px] w-[300px] rounded-md bg-gray-500 text-white">
+              Cambiar foto
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+      <div class="flex justify-center">
+        <div className="mb-4">
+          <button
+            className="h-[48px] w-[400px] rounded-md bg-gray-500 text-white"
+            onClick={() => navigate("/login")}
+          >
+            Ver billetera
+          </button>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div className="mb-4">
+          <button className="h-[48px] w-[400px] rounded-md bg-purple-500 text-white">
+            Confirmar cambios
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- puravidanft/src/Pages/NFTDetails/index.js
+++ puravidanft/src/Pages/NFTDetails/index.js
@@ -2,9 +2,11 @@
 import { useSelector } from "react-redux";*/
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../Component/Navbar/index.js";
+import React from "react";
 
 export default function NFTDetails() {
   const navigate = useNavigate();
+  const [showModal, setShowModal] = React.useState(false);
   /*  const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const theme = useSelector((state) => state.app.theme);*/
@@ -28,7 +30,7 @@ export default function NFTDetails() {
           </div>
         </div>
 
-        <div className="p-4 relative -right-1/3">
+        <div className="p-2 relative -right-1/3 ">
           <div className="p-8">
             <div className="mb-8 ">
               <div className="border-2 border-gray-400 bg-gray-300 text-gray-700 p-2 h-450 w-450">
@@ -37,13 +39,60 @@ export default function NFTDetails() {
                 <h1 className="text-2xl mb-3">Categoría: Futurismo</h1>
                 <h1 className="text-2xl mb-3">Precio: $9000</h1>
 
-                <div className="mb-4 flex justify-center">
+                <div className="mb-12 flex justify-center">
+                <>
+                <button className="h-[48px] w-[300px] rounded-md bg-purple-500 text-white" type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Comprar
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    ¿Desea comprar este NFT?
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto ">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Precio del NFT: 200
+                  </p>
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Saldo se de billetera: $6000
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex justify-center items-center  p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="h-[48px] w-[300px] rounded-md bg-purple-500 text-white"
-                    onClick={() => navigate("/")}
+                    className=" h-[48px] w-[150px] rounded-md bg-purple-500 text-white  background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
                   >
-                    Comprar
+                    Comprar 
                   </button>
+                  <button
+                    className=" h-[48px] w-[150px] rounded-md bg-gray-500 text-white font-bold uppercase text-sm px-6 py-3 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+       </>
                 </div>
               </div>
             </div>
